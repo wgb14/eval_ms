ce16fab7b499da150413d2dec692225174057d50
Guanbo Wang <wgb14@outlook.com>
2023-08-18 23:59:12 +0800

    Added MBPP eval script

---
+++ eval_ms.py
@@ -0,0 +1,46 @@
+import argparse
+import jsonlines
+
+
+def save_jsonl(data, path):
+    with jsonlines.open(path, "w") as writer:
+        writer.write_all(data)
+
+def eval_mbpp(args):
+    output = []
+    with jsonlines.open(args.input, "r") as reader:
+        for line in reader:
+            line["correctness"] = []
+            code = f"{line['response_text']}\n"
+            for item in line["test_list"]:
+                code_to_run = code + f"{item}\n"
+                try:
+                    exec(code_to_run)
+                    line["correctness"].append("correct")
+                except:
+                    line["correctness"].append("incorrect")
+            line["accuracy"] = sum([1 if x == "correct" else 0 for x in line["correctness"]]) / len(line["correctness"])
+            line["total_throughput"] = line["total_tokens"] * 1000 / line["latency"]
+            line["completion_thoroughput"] = line["completion_tokens"] * 1000 / line["latency"]
+            output.append(line)
+    save_jsonl(output, args.output)
+
+def eval_gsm(args):
+    pass
+
+def main(args):
+    if args.dataset == "mbpp":
+        eval_mbpp(args)
+    elif args.dataset == "gsm":
+        eval_gsm(args)
+    else:
+        raise ValueError("Dataset not supported")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", required=True)
+    parser.add_argument("--output", required=True)
+    parser.add_argument("--dataset", required=True)
+    args = parser.parse_args()
+    main(args)
